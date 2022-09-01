3f9f8200398af6e92c91b573ddad5fd23cec36e8
AyelenChamorro <ayelenchamorro31@hotmail.com>
2022-09-01 08:04:39 -0300

    landing page

---
+++ gulp/run_gulp_init.bat
@@ -0,0 +1,11 @@
+@ECHO OFF 
+:: Run gulp init
+TITLE Run gulp init
+ECHO ============================
+ECHO ENTER gulp init
+ECHO ============================
+echo %cd%
+ECHO ============================
+ECHO RUN gulp init
+ECHO ============================
+gulp init
---
+++ gulp/run_gulp_watch.bat
@@ -0,0 +1,11 @@
+@ECHO OFF 
+:: Run gulp watch
+TITLE Run gulp watch
+ECHO ============================
+ECHO ENTER gulp watch
+ECHO ============================
+echo %cd%
+ECHO ============================
+ECHO RUN gulp watch
+ECHO ============================
+gulp watch
